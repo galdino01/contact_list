c58c96b9771f1c1a4b8f7d27d9a0475ee28d100f
galdino01 <lucaspolucena@hotmail.com>
2022-08-23 12:40:19 -0300

    Adding dialog to discard changes

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -42,102 +42,137 @@ class _ContactPageState extends State<ContactPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          _updatedContact.name.isNotEmpty
-              ? _updatedContact.name
-              : 'Novo Contato',
+    return WillPopScope(
+      onWillPop: _requestPop,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            _updatedContact.name.isNotEmpty
+                ? _updatedContact.name
+                : 'Novo Contato',
+          ),
+          backgroundColor: Colors.blue,
+          centerTitle: true,
         ),
-        backgroundColor: Colors.blue,
-        centerTitle: true,
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          if (_updatedContact.phone.isEmpty || _updatedContact.phone == '') {
-            FocusScope.of(context).requestFocus(_phoneFocus);
-          }
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            if (_updatedContact.phone.isEmpty || _updatedContact.phone == '') {
+              FocusScope.of(context).requestFocus(_phoneFocus);
+            }
 
-          if (_updatedContact.email.isEmpty || _updatedContact.email == '') {
-            FocusScope.of(context).requestFocus(_emailFocus);
-          }
+            if (_updatedContact.email.isEmpty || _updatedContact.email == '') {
+              FocusScope.of(context).requestFocus(_emailFocus);
+            }
 
-          if (_updatedContact.name.isEmpty || _updatedContact.name == '') {
-            FocusScope.of(context).requestFocus(_nameFocus);
-          }
+            if (_updatedContact.name.isEmpty || _updatedContact.name == '') {
+              FocusScope.of(context).requestFocus(_nameFocus);
+            }
 
-          if (_updatedContact.name.isNotEmpty &&
-              _updatedContact.name != '' &&
-              _updatedContact.email.isNotEmpty &&
-              _updatedContact.email != '' &&
-              _updatedContact.phone.isNotEmpty &&
-              _updatedContact.phone != '') {
-            Navigator.pop(context, _updatedContact);
-          }
-        },
-        backgroundColor: Colors.blue,
-        child: const Icon(Icons.save),
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10),
-        child: Column(
-          children: [
-            GestureDetector(
-              child: Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: _updatedContact.image != ''
-                        ? FileImage(File(_updatedContact.image))
-                        : const AssetImage('assets/images/user.png')
-                            as ImageProvider,
-                    // https://www.flaticon.com/free-icons/person // person icons // Person icons created by Ilham Fitrotul Hayat - Flaticon
+            if (_updatedContact.name.isNotEmpty &&
+                _updatedContact.name != '' &&
+                _updatedContact.email.isNotEmpty &&
+                _updatedContact.email != '' &&
+                _updatedContact.phone.isNotEmpty &&
+                _updatedContact.phone != '') {
+              Navigator.pop(context, _updatedContact);
+            }
+          },
+          backgroundColor: Colors.blue,
+          child: const Icon(Icons.save),
+        ),
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: [
+              GestureDetector(
+                child: Container(
+                  width: 150,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: _updatedContact.image != ''
+                          ? FileImage(File(_updatedContact.image))
+                          : const AssetImage('assets/images/user.png')
+                              as ImageProvider,
+                      // https://www.flaticon.com/free-icons/person // person icons // Person icons created by Ilham Fitrotul Hayat - Flaticon
+                    ),
                   ),
                 ),
               ),
-            ),
-            TextField(
-              controller: _nameController,
-              focusNode: _nameFocus,
-              decoration: const InputDecoration(
-                labelText: 'Nome',
+              TextField(
+                controller: _nameController,
+                focusNode: _nameFocus,
+                decoration: const InputDecoration(
+                  labelText: 'Nome',
+                ),
+                onChanged: (name) {
+                  _userUpdated = true;
+                  setState(() {
+                    _updatedContact.name = name;
+                  });
+                },
               ),
-              onChanged: (name) {
-                _userUpdated = true;
-                setState(() {
-                  _updatedContact.name = name;
-                });
-              },
-            ),
-            TextField(
-              controller: _emailController,
-              focusNode: _emailFocus,
-              decoration: const InputDecoration(
-                labelText: 'Email',
+              TextField(
+                controller: _emailController,
+                focusNode: _emailFocus,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                ),
+                onChanged: (email) {
+                  _userUpdated = true;
+                  _updatedContact.email = email;
+                },
+                keyboardType: TextInputType.emailAddress,
               ),
-              onChanged: (email) {
-                _userUpdated = true;
-                _updatedContact.email = email;
-              },
-              keyboardType: TextInputType.emailAddress,
-            ),
-            TextField(
-              controller: _phoneController,
-              focusNode: _phoneFocus,
-              decoration: const InputDecoration(
-                labelText: 'Telefone',
+              TextField(
+                controller: _phoneController,
+                focusNode: _phoneFocus,
+                decoration: const InputDecoration(
+                  labelText: 'Telefone',
+                ),
+                onChanged: (phone) {
+                  _userUpdated = true;
+                  _updatedContact.phone = phone;
+                },
+                keyboardType: TextInputType.phone,
               ),
-              onChanged: (phone) {
-                _userUpdated = true;
-                _updatedContact.phone = phone;
-              },
-              keyboardType: TextInputType.phone,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<bool> _requestPop() {
+    if (_userUpdated) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text('Descartar Alterações?'),
+            content: const Text('Se sair, as alterações serão perdidas!'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text('Cancelar'),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                  Navigator.pop(context);
+                },
+                child: const Text('Sim'),
+              ),
+            ],
+          );
+        },
+      );
+      return Future.value(false);
+    } else {
+      return Future.value(true);
+    }
   }
 }
